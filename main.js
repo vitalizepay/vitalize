f55f3b6ad4518579333cae647aa777c97a6717c6
vitalizepay <vitalizepay@gmail.com>
2025-07-09 13:45:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Navbar background change on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled', 'shadow');
+    } else {
+        navbar.classList.remove('navbar-scrolled', 'shadow');
+    }
+});
